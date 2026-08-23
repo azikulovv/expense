df4ed15a429e0e2ab449284d5708c98868032e77
future <azikulov@proton.me>
2026-08-23 22:07:50 +0500

    feat: обновить логику вычисления доходов и расходов в useExpenseStats

--- app/entity/expense/model/useExpenseStats.ts
+++ app/entity/expense/model/useExpenseStats.ts
@@ -3,26 +3,30 @@ import { useExpenseList } from "~/entity/expense";
 export const useExpenseStats = () => {
   const { list } = useExpenseList();
 
-  const outcoming = computed(() =>
+  const totalIncome = computed(() =>
+    list.value
+      ?.filter((item) => item.type === "income")
+      ?.map((item) => item.amount)
+      ?.reduce((prev, current) => prev + current, 0),
+  );
+
+  const totalExpense = computed(() =>
     list.value
       ?.filter((item) => item.type === "expense")
       ?.map((item) => item.amount)
       ?.reduce((prev, current) => prev + current, 0),
   );
 
-  const incoming = computed(
-    () =>
-      list.value
-        ?.filter((item) => item.type === "income")
-        ?.map((item) => item.amount)
-        ?.reduce((prev, current) => prev + current, 0) - outcoming.value,
-  );
+  const remaining = computed(() => totalIncome.value - totalExpense.value);
 
   const percentage = computed(() => {
-    if (!incoming.value) return 0;
+    if (!totalIncome.value) return 0;
 
-    return Math.min(Math.round((outcoming.value / incoming.value) * 100), 100);
+    return Math.min(
+      Math.round((totalExpense.value / totalIncome.value) * 100),
+      100,
+    );
   });
 
-  return { incoming, outcoming, percentage };
+  return { totalIncome, totalExpense, remaining, percentage };
 };
