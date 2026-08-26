0ccd69a6b011fd8d44629e58063e648fe09f32a1
future <azikulov@proton.me>
2026-08-26 13:11:04 +0500

    fix: удалить нижний отступ в навигации в компоненте bottom-navigation.vue

--- app/widget/bottom-navigation/ui/bottom-navigation.vue
+++ app/widget/bottom-navigation/ui/bottom-navigation.vue
@@ -15,7 +15,7 @@ const items = [
 
 <template>
   <nav
-    class="fixed bottom-5 left-1/2 z-50 w-[calc(100%-2.5rem)] max-w-97.5 -translate-x-1/2 rounded-3xl border border-white/80 bg-white/40 p-1.5 backdrop-blur-2xl pb-[calc(0.375rem+env(safe-area-inset-bottom))]"
+    class="fixed bottom-5 left-1/2 z-50 w-[calc(100%-2.5rem)] max-w-97.5 -translate-x-1/2 rounded-3xl border border-white/80 bg-white/40 p-1.5 backdrop-blur-2xl"
   >
     <div class="grid grid-cols-2 gap-1.5">
       <BottomNavigationItem
